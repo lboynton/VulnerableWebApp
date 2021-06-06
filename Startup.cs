35336373287e4414865bb474899c35fbbcdedc0f
Lee Boynton <lee.boynton@truelayer.com>
2021-06-06 20:44:20 +0100

    Add stored XSS vuln

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,10 @@ namespace VulnerableWebApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
 
             app.UseMvc();
