267599efabb871b48836202775436ace20fa6875
Lee Boynton <lee.boynton@truelayer.com>
2022-05-26 15:15:17 +0100

    Make route different

--- Controllers/Api/Posts.cs
+++ Controllers/Api/Posts.cs
@@ -27,7 +27,7 @@ namespace VulnerableWebApp.Controllers.Api
             return _context.Posts.FromSqlRaw("Select * from Posts where Id = " + id).FirstOrDefault();
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("mk2/{id}")]
         public Post GetPostMk2(string id)
         {
             return _context.Posts.FromSqlRaw("Select * from Posts where Id IN (" + id + ")").FirstOrDefault();
